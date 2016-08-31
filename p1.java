2926f7c31943e8045936444d8e9d0bcededa6d6a
ranjuslm <ran853@gmail.com>
2016-09-01 00:38:18 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class p1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+StringBuffer sb=new StringBuffer();
+sb.append(s);
+System.out.println(sb.reverse());
+}
+}
